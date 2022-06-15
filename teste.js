56e05ea94c104c8529df36a6e6a50d754bab4d66
Samuel Pereira Brandão <samuelpbrandao58@gmail.com>
2022-06-15 01:43:12 -0300

    update

---
+++ teste.js
@@ -0,0 +1,15 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+    imgs.style.transform =`translateX(${-idx * 500}px`;
+}
+
+setInterval(carrossel,2500);
